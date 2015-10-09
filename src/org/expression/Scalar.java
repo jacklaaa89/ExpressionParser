76319f8cfbb956ca3c43576dcde73ddc84541d47
Jack Timblin <jacktimblin@gmail.com>
2015-10-09 16:00:57 +0100

    Added documentation to the Scalar class.

--- src/org/expression/Scalar.java
+++ src/org/expression/Scalar.java
@@ -6,36 +6,80 @@ import org.expression.computation.Functions;
 import java.math.MathContext;
 
 /**
- *
- * @author jacktimblin
+ * A representation of a Scalar value. It is effectively a container for
+ * a Double values which provide OOP design patterns for performing arithmetic
+ * functions.
+ * @author Jack Timblin
  */
 public class Scalar implements Arithmetic<Arithmetic>  {
     
+    /**
+     * Generates a new Scalar with a zero value.
+     */
     public static final Scalar ZERO = new Scalar(0d);
+    
+    /**
+     * Generates a new Scalar with a value of one.
+     */
     public static final Scalar ONE  = new Scalar(1d);
+    
+    /**
+     * Generates a new Scalar with a value of two.
+     */
     public static final Scalar TWO  = new Scalar(2d); 
     
+    /**
+     * The MathContext to use.
+     */
     private final MathContext mc;
+    
+    /**
+     * The value of this Scalar.
+     */
     public final double value;
     
+    /**
+     * Initializes a Scalar value using a given MathContext,
+     * using a DECIMAL32 MathContext.
+     * @param val the initial value of this scalar.
+     */
     public Scalar(double val) {
         this.value = val;
         this.mc = MathContext.DECIMAL32;
     }
     
+    /**
+     * Initializes a Scalar value using a given MathContext.
+     * @param value the initial value of this scalar to be parsed into a double.
+     */
     public Scalar(String value) {
         this(Double.parseDouble(value));
     }
     
+    /**
+     * Initializes a Scalar value using a given MathContext.
+     * @param val the initial value of this scalar.
+     * @param mc A MathContext to use.
+     */
     public Scalar(double val, MathContext mc) {
         this.value = val;
         this.mc = mc;
     }
     
+    /**
+     * Generates a Scalar with a random value between 0 and 1, 
+     * using a DECIMAL32 MathContext.
+     * @return the random Scalar value.
+     */
     public static Scalar random() {
         return Scalar.random(MathContext.DECIMAL32);
     }
     
+    /**
+     * Generates a Scalar with a random value between 0 and 1.
+     * @param mc A MathContext to use.
+     * @return the random Scalar value.
+     */
     public static Scalar random(MathContext mc) {
         return new Scalar(Math.random(), mc);
     }
@@ -54,6 +98,11 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         return (Arithmetic) data.apply(h);
     }
     
+    /**
+     * Perform A*C to give C.
+     * @param value the data.
+     * @return the computed data structure.
+     */
     public Scalar multiply(Scalar value) {
         return new Scalar(((Scalar)value).value*this.value, mc);
     }
@@ -67,6 +116,11 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         return (Arithmetic) data.apply(h);
     }
     
+    /**
+     * Perform A/C to give C.
+     * @param data the data.
+     * @return the computed data structure.
+     */
     public Scalar divide(Scalar data) {
         return new Scalar(this.value/((Scalar)data).value, mc);
     }
@@ -80,6 +134,11 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         return (Arithmetic) data.apply(h);
     }
     
+    /**
+     * Perform A+C to give C.
+     * @param data the data.
+     * @return the computed data structure.
+     */
     public Scalar add(Scalar data) {
         return new Scalar(this.value+((Scalar)data).value, mc);
     }
@@ -93,6 +152,11 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         return (Arithmetic) data.apply(h);
     }
     
+    /**
+     * Perform A-B to give C.
+     * @param value the data.
+     * @return the computed data structure.
+     */
     public Scalar subtract(Scalar value) {
         return new Scalar(this.value-((Scalar)value).value, mc);
     }
@@ -106,6 +170,11 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         return (Arithmetic) data.apply(h);
     }
     
+    /**
+     * Perform A%B to give C.
+     * @param data the data.
+     * @return the computed data structure.
+     */
     public Scalar remainder(Scalar data) {
         double d = ((Scalar) data).value;
         return new Scalar(this.value % d);
@@ -126,10 +195,19 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         return this;
     }
     
+    /**
+     * Returns this {@code Scalar} as a double.
+     * This function works as a placeholder for {@code this.value}.
+     * @return this scalar represented as a double.
+     */
     public double doubleValue() {
         return this.value;
     }
     
+    /**
+     * returns the square root of this value.
+     * @return  the sqrt of this value.
+     */
     public Scalar sqrt() {
         return new Scalar(Math.sqrt(value));
     }
@@ -185,11 +263,18 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         Double d = ((Scalar)o).value;
         return d.compareTo(this.value);
     }
-
+    
+    /**
+     * return the value of this {@code Scalar} represented as an int.
+     * @return this scalar as an integer
+     */
     public int intValue() {
         return (int) Math.round(value);
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object o) {
         if(!(o instanceof Scalar)) return false;
@@ -203,6 +288,11 @@ public class Scalar implements Arithmetic<Arithmetic>  {
         return hash;
     }
     
+    /**
+     * returns this raised to the power of {@code n}.
+     * @param n the exponent
+     * @return the value of {@code this}<sup>{@code n}</sup>.
+     */
     public Scalar power(Scalar n) {
         return new Scalar(Math.pow(value, n.value));
     }
