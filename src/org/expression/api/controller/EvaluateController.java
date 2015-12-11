40ec468925c38bac6dcf57ad04016beac5a07ccb
Jack Timblin <jacktimblin@gmail.com>
2015-12-11 09:17:24 +0000

    Added the evaluation controller which evaluates input.

---
+++ src/org/expression/api/controller/EvaluateController.java
@@ -0,0 +1,53 @@
+package org.expression.api.controller;
+
+import org.expression.Context;
+import org.expression.Expression;
+import org.expression.api.DependencyInjector;
+import org.expression.api.annotation.HttpMethod;
+import org.expression.api.exception.DispatchException;
+import org.expression.http.RequestType;
+import org.expression.http.StatusCode;
+import org.expression.http.data.Data;
+
+/**
+ *
+ * @author Jack
+ */
+public class EvaluateController extends Controller {
+
+    public EvaluateController(DependencyInjector di) {
+        super(di);
+    }
+    
+    @HttpMethod({RequestType.POST})
+    public Data indexAction() throws DispatchException {
+        Data d = this.getRequest().getData();
+        if(!d.hasKey("Request")) {
+            return new Data();
+        }
+        d = d.get("Request");
+        if(!d.hasKey("Expression")) {
+            return new Data();
+        }
+        Expression e = new Expression();
+        String ex = d.get("Expression", String.class);
+        if(ex == null) {
+            return new Data();
+        }
+        Data re = new Data();
+        e.setExpression(ex);
+        try {
+            Context c = e.eval();
+            System.out.println(c);
+            re.set("Expression", c.getExpression());
+            re.set("Result", c.getValue().toString());
+            return re;
+        } catch (RuntimeException er) {
+            throw DispatchException.newInstance(
+                    StatusCode.BAD_REQUEST, 
+                    er.getMessage(), 
+                    er
+            );
+        }
+    }
+}
